0ddad1b1c707e1ae02302daafaa006a15310531d
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-04-07 14:58:38 +0000

    fixed js and added 2nd function

--- js/script.js
+++ js/script.js
@@ -7,8 +7,22 @@ function calculate () {
   let pounds = parseFloat(document.getElementById('inputPounds').value)
 
   // process
-  let kilos = pounds * 2.2046
+  let kilos = pounds / 2.2046
 
   // output
-  document.getElementById('answers').innerHTML = 'The amount of pounds in kilos is ' + kilos + ' kg'
+  document.getElementById('answers').innerHTML = 'The amount of pounds in kilos is ' + kilos.toFixed(2) + ' kg'
+}
+
+/**
+ * This function calculates kilos to pounds
+ */
+function calculate2 () {
+  // input
+  let kilos = parseFloat(document.getElementById('inputKilos').value)
+
+  // process
+  let pounds = kilos * 2.2046
+
+  // output
+  document.getElementById('answers2').innerHTML = 'The amount of kilos in pounds is ' + pounds.toFixed(2) + ' pounds'
 }
