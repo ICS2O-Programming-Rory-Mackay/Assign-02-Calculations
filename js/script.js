bde8dbcb324785c97b366239da6efeaf0710d707
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-04-07 03:10:23 +0000

    attempted to fix js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
- function weightConverter(valNum) {
-      document.getElementById("outputKilograms").innerHTML=valNum/2.2046;
-    }
-
 'use strict'
 /**
  * This function calculates pounds to kilos
  */
 function calculate () {
   // input
-  const pounds = parseFloat(document.getElementById('length-of-rectangle').value)
+  let pounds = parseFloat(document.getElementById('inputPounds').value)
 
   // process
-  const kilos = pounds * 2.2046
+  let kilos = pounds * 2.2046
 
   // output
-  document.getElementById('pounds').innerHTML = 'The amount of pounds in kilos is ' + pounds + ' kg'
+  document.getElementById('kilos').innerHTML = 'The amount of pounds in kilos is ' + kilos + ' kg'
 }
